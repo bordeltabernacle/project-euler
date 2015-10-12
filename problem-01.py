5d34d6518aaa76961d4c477930533bc386ace642
Rob Phoenix <robertphoenix@gmail.com>
2015-10-12 12:44:47 +0100

    refactored problem-01.py with a list comprehension

--- problem-01.py
+++ problem-01.py
@@ -2,8 +2,4 @@ def sum_mul_three_five(end_num):
     """
     Returns the sum of all the multiples of 3 or 5 below end_num
     """
-    total = []
-    for num in range(end_num):
-        if num % 3 == 0 or num % 5 == 0:
-            total.append(num)
-    return sum(total)
+    return sum([num for num in range(end_num) if not num % 3 or not num % 5])
