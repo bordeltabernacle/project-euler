d89189ffa46b0031bec3ee87da915d852535267a
Rob Phoenix <robertphoenix@gmail.com>
2015-10-12 10:54:53 +0100

    refactored problem-01.py

--- problem-01.py
+++ problem-01.py
@@ -1,5 +1,9 @@
-total = 0
-for num in range(1000):
-    if (num % 3 == 0) or (num % 5 == 0):
-        total += num
-print "Total: ", total
+def sum_mul_three_five(end_num):
+    """
+    Returns the sum of all the multiples of 3 or 5 below end_num
+    """
+    total = []
+    for num in range(end_num):
+        if num % 3 == 0 or num % 5 == 0:
+            total.append(num)
+    return sum(total)
