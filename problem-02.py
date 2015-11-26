f87ca02f3564b010b9de3690de6613af707735e8
Rob Phoenix <robertphoenix@gmail.com>
2015-11-26 17:31:41 +0000

    refactored problem-02.py

--- problem-02.py
+++ problem-02.py
@@ -1,17 +1,13 @@
-def fibonacci_sequence(start_number, end_number):
-    a, b = start_number, start_number + 1
-    yield a
-    yield b
-    while a < end_number:
+def fibonacci(n):
+    a, b = 0, 1
+    while a <= n:
+        yield a
         a, b = b, a + b
-        yield b
 
-def sum_of_even_fibonacci_numbers(start_number, end_number):
-    total = 0
-    for number in fibonacci_sequence(start_number, end_number):
-        if number % 2 == 0:
-            total += number
-    return total
 
-result = sum_of_even_fibonacci_numbers(1, 4000000)
-print "\nThe sum of the even-valued terms is: ", result, "\n"
+def sum_even(coll):
+    return sum([x for x in coll if x % 2 == 0])
+
+
+def sum_even_fibonacci(n):
+    return sum_even(list(fibonacci(n)))
--- problem-03.py
+++ problem-03.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 def prime(n):
     if n > 2 and n % 2 == 0 or \
        n > 3 and n % 3 == 0 or \
@@ -15,9 +18,8 @@ def largest_prime_factor(n):
         if x > 0 and n % int(x) == 0 and prime(x):
             if largest is None or x > largest:
                 largest = x
-                print largest
     print "Largest is %d" % largest
 
 
-largest_prime_factor(600851475143)
-# largest_prime_factor(13195)
+# largest_prime_factor(600851475143)
+largest_prime_factor(13195)
