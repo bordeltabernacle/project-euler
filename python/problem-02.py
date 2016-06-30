a9cc5f865c376f3a89f69250d26a14703047c991
Rob Phoenix <robertphoenix@gmail.com>
2016-06-30 10:34:08 +0100

    minor changes to problem-02

--- python/problem-02.py
+++ python/problem-02.py
@@ -5,9 +5,7 @@ def fibonacci(n):
         a, b = b, a + b
 
 
-def sum_even(coll):
-    return sum([x for x in coll if x % 2 == 0])
-
-
 def sum_even_fibonacci(n):
-    return sum_even(list(fibonacci(n)))
+    return sum(x for x in fibonacci(n) if not x % 2)
+
+print(sum_even_fibonacci(4000000))
