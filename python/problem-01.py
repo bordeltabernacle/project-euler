8c37e3516a7f147ad77460fc00a670dcbf9badce
Rob Phoenix <robertphoenix@gmail.com>
2016-06-30 09:13:40 +0100

    minor edits to ex1

--- python/problem-01.py
+++ python/problem-01.py
@@ -1,5 +1,5 @@
-def sum_mul_three_five(end_num):
+def sum_mul_three_five(end):
     """
     Returns the sum of all the multiples of 3 or 5 below end_num
     """
-    return sum([num for num in range(end_num) if not num % 3 or not num % 5])
+    return sum(n for n in range(end) if not n % 3 or not n % 5)
